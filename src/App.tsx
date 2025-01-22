e59c3b52b81b43179aeef26c91b9295e50940b55
AL Riyad <imalriyad@gmail.com>
2025-01-22 13:57:16 +0600

    extension starter kit added with react+tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <div className="w-full flex justify-center align-middle items-center h-screen">
+       <h1>Extenstion template</h1>
+    </div>
+  );
+}
+
+export default App;
